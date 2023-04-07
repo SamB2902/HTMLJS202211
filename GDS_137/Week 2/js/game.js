3ac4df864ae5752cfabb36077eaee1ed1214f128
SamB2902 <sebianchi@email.neit.edu>
2023-04-07 13:09:08 -0400

    merge

---
+++ GDS_137/Week 2/js/game.js
@@ -0,0 +1,85 @@
+//Declare my variables
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var timer = setInterval(animate, interval);
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+var player1;
+
+//This is used to stop the player from moving through obstacles.
+//var prevX;
+
+	//Instantiate the Player
+	player = new GameObject();
+	player.vx = -2;
+	player.vy = -2;	
+	
+//Instantiate Player 1
+	var player1 = new GameObject();
+	player1.x = 5;
+	player1.width = 10;
+	player1.height = 150;
+	player1.color = "blue";
+	
+	
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	player1.move();
+	player.move();
+	
+		//ball collision
+		if(player.x < 0 + player.width/2)
+		{
+			player.x = 0 + player.width/2;
+			player.vx = -player.vx;
+			player.color =  "#ff8000";
+		}
+		if(player.x > 925 + player.width/2)
+		{
+			player.x = 925 + player.width/2;
+			player.vx = -player.vx;
+			player.color =  "#ff00ff";
+		}
+	
+		if(player.y < 0 + player.height/2)
+		{
+			player.y = 0 + player.height/2;
+			player.vy = -player.vy;
+			player.color =  "#00ff00";
+		}
+		if(player.y > 705 + player.height/2)
+		{
+			player.y = 705 + player.height/2;
+			player.vy = -player.vy;
+			player.color =  "#00ffff";
+		}
+	
+	//player 1 collision
+	if(player1.y < 0 + player1.height/2)
+	{
+		player1.y = 0 + player1.height/2;
+		player1.vy = -player1.vy;
+	}
+	if(player1.y > 650 + player1.height/2)
+	{
+		player1.y = 650 + player1.height/2;
+		player1.vy = -player1.vy;
+	}
+	//player 1 controls
+	if(w)
+	{
+		player1.y += -2
+	}
+	if(s)
+	{
+		player1.y += 2
+	}
+player1.drawRect();
+player.drawCircle();
+}
+
