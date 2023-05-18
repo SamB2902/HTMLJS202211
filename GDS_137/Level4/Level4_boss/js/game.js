a0ec9aa7f23cd5b1f7fd1bec3190dbce0ad6a6e2
SamB2902 <sebianchi@email.neit.edu>
2023-05-18 12:33:31 -0400

    Urgent

--- GDS_137/Level4/Level4_boss/js/game.js
+++ GDS_137/Level4/Level4_boss/js/game.js
@@ -5,20 +5,21 @@ var timer = setInterval(animate, interval);
 var fX = .85;
 var fY = .97;
 var gravity = 1;
-var jumpCount = 1;
 
-enemy = new GameObject({color:"#990000", height:50, width:50, x:canvas.width+25, y:canvas.height-25, vx:-3});
+enemy = new GameObject({color:"#990000", height:100, width:50, x:canvas.width+25, y:canvas.height-25, vx:-3, angle:-90});
 player = new GameObject({color:"#0066ff", height:50, width:50, y:675});
 health = new GameObject({width:200, height:35, color:"#00ff00", x:10, y:12});
-healthStroke = new GameObject({x:9, y:11, color:"black", height:37, width: 202})
+healthStroke = new GameObject({x:9, y:11, color:"black", height:37, width: 202});
+platform = new GameObject({color: "black", width: canvas.width, height:1, y: 481});
+powerUp = new GameObject({color:"#007676", height:20, width:30, x:850})
 
 function animate(){
     context.clearRect(0,0,canvas.width, canvas.height);	
     
     player.move();
-    enemy.move(); 
+    //enemy.move(); 
     player.vx *= fX;
-	player.vy *= fY;
+	player.vy *= fY;;;
     player.vy += gravity;
     
     if(player.x < 0 + player.width/2)
@@ -34,12 +35,6 @@ function animate(){
     if (player.y > 650 + player.height/2)
     {
         player.y = 650 + player.height/2;
-        player.vy = 0;
-        player.canJump = true;
-    }
-    if (player.y > 630 + player.height/2 && jumpCount >= 2)
-    {
-        player.y = 630 + player.height/2;
         player.vy = 0;
         player.canJump = true;
     }
@@ -61,27 +56,26 @@ function animate(){
          context.textAlign = "center";
          context.fillText("GAME OVER", canvas.width/2,canvas.height/2)}
     
-    if(w && player.canJump && player.vy ==0 && jumpCount == 1)
+    if(w && player.canJump && player.vy ==0)
 	{
 		player.canJump = false;
-		player.vy += player.jumpHeight;
-        jumpCount++
-        
+		player.vy += player.jumpHeight; 
 	}
-    if(w && player.canJump && player.vy ==0 && jumpCount >= 2)
-	{
-		player.canJump =false;
-		player.vy += player.jumpHeight*1.5;
-        jumpCount = 1 
-	}
-
+    
+    if(platform.hitTestPoint(player.bottom()) && w)
+    {
+        player.vy += player.jumpHeight;
+        player.canJump = false; 
+    } 
+    
     if(a)
 	{player.x += -2;}
 	if(d)
 	{player.x += 2;}
     
-    enemy.drawCircle();
+    enemy.drawTriangle();
     player.drawRect();
     health.drawRect(0,0);
     healthStroke.drawStrokeRect(0,0);
+    powerUp.drawCircle();
 }
