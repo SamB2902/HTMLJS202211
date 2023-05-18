45394ed2ad08851bcbe8290b266eadf4174ce3b7
SamB2902 <sebianchi@email.neit.edu>
2023-05-18 13:00:07 -0400

    mechanic 3

--- GDS_137/mechanicsBoss/js/game.js
+++ GDS_137/mechanicsBoss/js/game.js
@@ -11,7 +11,7 @@ player = new GameObject({color:"#0066ff", height:50, width:50, y:675});
 health = new GameObject({width:200, height:35, color:"#00ff00", x:10, y:12});
 healthStroke = new GameObject({x:9, y:11, color:"black", height:37, width: 202});
 platform = new GameObject({color: "black", width: canvas.width, height:1, y: 481});
-powerUp = new GameObject({color:"#007676", height:20, width:30, x:850})
+powerUp = new GameObject({color:"#00FFFF", height:20, width:30, x:850})
 
 function animate(){
     context.clearRect(0,0,canvas.width, canvas.height);	
@@ -55,23 +55,29 @@ function animate(){
          context.fillStyle = "#000000";
          context.textAlign = "center";
          context.fillText("GAME OVER", canvas.width/2,canvas.height/2)}
+
+         if(player.hitTestObject(powerUp))
+         {
+            powerUp.x = 1500;
+            if(health.width < 200)
+            {health.width = health.width + 25}
+         }
+            
     
     if(w && player.canJump && player.vy ==0)
 	{
 		player.canJump = false;
 		player.vy += player.jumpHeight; 
 	}
-    
+    if(a)
+	{player.x += -2;}
+	if(d)
+	{player.x += 2;} 
     if(platform.hitTestPoint(player.bottom()) && w)
     {
         player.vy += player.jumpHeight;
         player.canJump = false; 
     } 
-    
-    if(a)
-	{player.x += -2;}
-	if(d)
-	{player.x += 2;}
     
     enemy.drawTriangle();
     player.drawRect();
